5d188e38bea8c0a035f895cd611a376b7122193b
Valerio Faiuolo <olosnet.com@gmail.com>
2025-01-03 13:28:23 +0000

    Base per app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sidemenu',
+  webDir: 'www/browser'
+};
+
+export default config;
